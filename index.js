71fa09b6eb6519f0301232e169f067794790b195
gconnell29 <gconnelljr29@gmail.com>
2020-12-06 19:42:30 -0500

    completed Task 3

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+    let allFinalists = data.filter((item) => item.Stage == 'Final');
+    return allFinalists;
 }
 
 
@@ -42,8 +43,12 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, callback) {
+    let years = [];
+    callback(array).forEach(item => {
+        years.push(item.Year)
+    });
+    return years;
 }
 
 
